2268e8b88d9b4d79dfddbcebd425316f200e9876
Paul Davies <paul.davies@navetas.com>
2014-12-23 10:38:58 +0000

    Added scalatest dependency to project

--- project/build.scala
+++ project/build.scala
@@ -3,9 +3,15 @@ import Keys._
 
 object MathsProblemBuild extends Build
 {
+    lazy val sharedLibraryDependencies = Seq(
+        "org.scalatest" % "scalatest_2.11" % "2.2.1" % "test"
+    )
+
     lazy val root = Project(
         id = "MathsProblem",
         base = file( "." ),
-        settings = Project.defaultSettings
+        settings = Project.defaultSettings ++ Seq(
+            libraryDependencies ++= sharedLibraryDependencies
+        )
     )
 }
