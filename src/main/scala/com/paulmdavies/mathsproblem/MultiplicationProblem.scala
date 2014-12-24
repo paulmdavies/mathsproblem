fdfb1b5f92d20b50ed45ca14c8535ba5edb03ccf
Paul Davies <paul.davies@navetas.com>
2014-12-24 11:31:31 +0000

    Added MultiplicationProblem

---
+++ src/main/scala/com/paulmdavies/mathsproblem/MultiplicationProblem.scala
@@ -0,0 +1,13 @@
+package com.paulmdavies.mathsproblem
+
+object MultiplicationProblem
+{
+    def apply( left : Int, right : Int ) : MultiplicationProblem = new MultiplicationProblem( left, right )
+}
+
+class MultiplicationProblem private [mathsproblem]( private [mathsproblem] val left : Int, private [mathsproblem] val right : Int ) extends Problem
+{
+    val answer = left * right
+    
+    override def toString() : String = "%d x %d".format( left, right )
+}
---
+++ src/test/scala/com/paulmdavies/mathsproblem/MultiplicationProblemTests.scala
@@ -0,0 +1,30 @@
+package com.paulmdavies.mathsproblem
+
+import org.scalatest._
+import Matchers._
+import org.scalatest.prop._
+
+class MultiplicationProblemTests extends FlatSpec with Matchers with PropertyChecks
+{
+    it should "know the answer to the problem" in
+    {
+        forAll { ( left : Int, right : Int ) => {
+            MultiplicationProblem( left, right ).answer should equal ( left * right )
+        } }
+    }
+    
+    it should "be able to tell if a guess is correct" in
+    {
+        forAll { ( left : Int, right : Int ) => {
+            MultiplicationProblem( left, right ).guess( left * right ) should equal ( true )
+            MultiplicationProblem( left, right ).guess( left * right + 1 ) should equal ( false )
+        } }
+    }
+    
+    it should "serialise correctly" in
+    {
+        forAll { ( left : Int, right : Int ) => {
+            MultiplicationProblem( left, right ).toString should equal( "%d x %d".format( left, right ) )
+        } }
+    }
+}
