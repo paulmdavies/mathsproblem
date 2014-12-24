f1a0bda5b5eb7842bbef7ea492ed335635c278e3
Paul Davies <paul.davies@navetas.com>
2014-12-24 10:00:42 +0000

    Converted ProblemStats tests to property-driven approach

--- src/test/scala/com/paulmdavies/mathsproblem/stats/ProblemStatsTests.scala
+++ src/test/scala/com/paulmdavies/mathsproblem/stats/ProblemStatsTests.scala
@@ -2,37 +2,44 @@ package com.paulmdavies.mathsproblem.stats
 
 import org.scalatest._
 import Matchers._
+import org.scalatest.prop._
 import com.paulmdavies.mathsproblem._
 
-class ProblemStatsTests extends FlatSpec with Matchers 
+class ProblemStatsTests extends FlatSpec with Matchers with PropertyChecks 
 {
     it should "increment the count whenever problems are added" in
     {
         val ps = ProblemStats()
-        ps.count should equal( 0 )
-        ps.add( AdditionProblem( 1, 2 ), 3 )
-        ps.count should equal( 1 )
-        ps.add( AdditionProblem( 1, 2 ), 7 )
-        ps.count should equal( 2 )
+        forAll{ ( left : Int, right : Int ) => {
+            val count = ps.count
+            ps.add( AdditionProblem( left, right ), left + right )
+            ps.count should equal { count + 1 }
+            ps.add( AdditionProblem( left, right ), left + right + 1 )
+            ps.count should equal { count + 2 }
+        } }
     }
     
     it should "increment the correct count only when correctly-answered problems are added" in
     {
         val ps = ProblemStats()
-        ps.correctCount should equal( 0 )
-        ps.add( AdditionProblem( 1, 2 ), 3 )
-        ps.correctCount should equal( 1 )
-        ps.add( AdditionProblem( 1, 2 ), 7 )
-        ps.correctCount should equal( 1 )
+        forAll{ ( left : Int, right : Int ) => {
+            val correctCount = ps.correctCount
+            ps.add( AdditionProblem( left, right ), left + right )
+            ps.correctCount should equal { correctCount + 1 }
+            ps.add( AdditionProblem( left, right ), left + right + 1 )
+            ps.correctCount should equal { correctCount + 1 }
+        } }
     }
     
     it should "have incorrect count equal to total count minus correct count" in
     {
         val ps = ProblemStats()
-        ps.incorrectCount should equal( 0 )
-        ps.add( AdditionProblem( 1, 2 ), 3 )
-        ps.incorrectCount should equal( 0 )
-        ps.add( AdditionProblem( 1, 2 ), 7 )
-        ps.incorrectCount should equal( 1 )
+        forAll{ ( left : Int, right : Int ) => {
+            val incorrectCount = ps.incorrectCount
+            ps.add( AdditionProblem( left, right ), left + right )
+            ps.incorrectCount should equal { incorrectCount }
+            ps.add( AdditionProblem( left, right ), left + right + 1 )
+            ps.incorrectCount should equal { incorrectCount + 1 }
+        } }
     }
 }
