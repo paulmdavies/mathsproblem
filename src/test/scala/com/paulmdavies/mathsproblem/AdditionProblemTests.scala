5b285ee836f2669b3a81f2ed50573a4f277de36d
Paul Davies <paul.davies@navetas.com>
2014-12-24 09:47:52 +0000

    Converted AdditionProblem testing to property-driven approach

--- src/test/scala/com/paulmdavies/mathsproblem/AdditionProblemTests.scala
+++ src/test/scala/com/paulmdavies/mathsproblem/AdditionProblemTests.scala
@@ -2,27 +2,29 @@ package com.paulmdavies.mathsproblem
 
 import org.scalatest._
 import Matchers._
+import org.scalatest.prop._
 
-class AdditionProblemTests extends FlatSpec with Matchers
+class AdditionProblemTests extends FlatSpec with Matchers with PropertyChecks
 {
-    it should "be constructable from two integers" in
-    {
-        AdditionProblem( 1, 2 )
-    }
-    
     it should "know the answer to the problem" in
     {
-        AdditionProblem( 1, 2 ).answer should equal ( 3 )
+        forAll { ( left : Int, right : Int ) => {
+            AdditionProblem( left, right ).answer should equal ( left + right )
+        } }
     }
     
     it should "be able to tell if a guess is correct" in
     {
-        AdditionProblem( 1, 2 ).guess( 3 ) should equal ( true )
-        AdditionProblem( 1, 2 ).guess( 4 ) should equal ( false )
+        forAll { ( left : Int, right : Int ) => {
+            AdditionProblem( left, right ).guess( left + right ) should equal ( true )
+            AdditionProblem( left, right ).guess( left + right + 1 ) should equal ( false )
+        } }
     }
     
     it should "serialise correctly" in
     {
-        AdditionProblem( 1, 2 ).toString should equal( "1 + 2" )
+        forAll { ( left : Int, right : Int ) => {
+            AdditionProblem( left, right ).toString should equal( "%d + %d".format( left, right ) )
+        } }
     }
 }
