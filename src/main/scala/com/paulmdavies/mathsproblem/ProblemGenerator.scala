ed688b77d7c4e42c831c04a5df1e39ce5aeb4d02
Paul Davies <paul.davies@navetas.com>
2014-12-24 10:54:39 +0000

    Converted ProblemGenerator tests to property-driven approach
    Also, refactored to not allow full integer range of limits

--- src/main/scala/com/paulmdavies/mathsproblem/ProblemGenerator.scala
+++ src/main/scala/com/paulmdavies/mathsproblem/ProblemGenerator.scala
@@ -2,12 +2,16 @@ package com.paulmdavies.mathsproblem
 
 object ProblemGenerator
 {
+    private [mathsproblem] val LIMIT = 1000
+    
     def apply( lowerLimit : Int, upperLimit : Int ) : ProblemGenerator = new ProblemGenerator( lowerLimit, upperLimit )
 }
 
 class ProblemGenerator private [mathsproblem] ( private [mathsproblem] lowerLimit : Int, private [mathsproblem] upperLimit : Int )
 {
-    require( lowerLimit <= upperLimit, "Lower limit (%d) should be less than upper limit (%d)" )
+    require( lowerLimit < upperLimit, "Lower limit (%d) should be less than upper limit (%d)".format( lowerLimit, upperLimit ) )
+    require( lowerLimit >= -1 * ProblemGenerator.LIMIT, "Minimum value is -%d".format( ProblemGenerator.LIMIT ) )
+    require( upperLimit <= ProblemGenerator.LIMIT, "Maximum value is %d".format( ProblemGenerator.LIMIT ) )
     
     private [mathsproblem] def numberInRange() = scala.util.Random.nextInt( upperLimit - lowerLimit ) + lowerLimit
     
--- src/test/scala/com/paulmdavies/mathsproblem/ProblemGeneratorTests.scala
+++ src/test/scala/com/paulmdavies/mathsproblem/ProblemGeneratorTests.scala
@@ -1,20 +1,44 @@
 package com.paulmdavies.mathsproblem
 
 import org.scalatest._
+import org.scalatest.prop._
 import Matchers._
+import org.scalacheck.Gen
 
-class ProblemGeneratorTests extends FlatSpec with Matchers
+class ProblemGeneratorTests extends FlatSpec with Matchers with PropertyChecks
 {
+    val validLimits = for ( n <- Gen.choose( -1 * ProblemGenerator.LIMIT, ProblemGenerator.LIMIT ) ) yield n 
+    
     it should "generate a random problem within the given bounds" in
     {
-        val generator = ProblemGenerator( 0, 10 )
-        val problem = generator.apply()
-        ( 0 until 10 ) should contain ( problem.left )
-        ( 0 until 10 ) should contain ( problem.right )
+        forAll ( validLimits, validLimits ) { ( low : Int, high : Int ) => whenever ( low < high ) { 
+            val generator = ProblemGenerator( low, high )
+            val problem = generator.apply()
+            problem.left should be >= low
+            problem.left should be < high
+            problem.right should be >= low
+            problem.right should be < high
+        } }
     }
     
     it should "fail if lower limit is less than upper limit" in
     {
-        an [IllegalArgumentException] should be thrownBy ProblemGenerator( 1, 0 )
+        forAll ( validLimits, validLimits ) { ( low : Int, high : Int ) => whenever ( !( low < high ) ) {
+            an [IllegalArgumentException] should be thrownBy ProblemGenerator( low, high )
+        } }
+    }
+    
+    it should "fail if the lower limit is less than the minimum limit" in
+    {
+        forAll { ( low : Int ) => whenever ( low < -1 * ProblemGenerator.LIMIT ) {
+            an [IllegalArgumentException] should be thrownBy ProblemGenerator( low, 0 )
+        } }
+    }
+    
+    it should "fail if the upper limit is more than the maximum limit" in
+    {
+        forAll { ( high : Int ) => whenever ( high > ProblemGenerator.LIMIT ) {
+            an [IllegalArgumentException] should be thrownBy ProblemGenerator( 0, high )
+        } }
     }
 }
